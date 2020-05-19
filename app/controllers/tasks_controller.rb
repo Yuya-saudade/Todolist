1f71177c5a0b831c47f09ae71a37048662192b21
Yuya-Saudade <saudade.bonfire.6002@gmail.com>
2020-05-19 21:18:00 +0900

    5月19日最終

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,13 @@
 class TasksController < ApplicationController
   def index
-    @task = Task.all
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def new
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "tasks/index" => "tasks#index"
+
   get "tasks/:id" => "tasks#show"
 
   get "/" => "home#top"
