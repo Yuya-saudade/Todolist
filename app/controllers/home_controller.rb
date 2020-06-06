cb27d989ecbaa2807550d74842e1d77fbb5547a4
Yuya-Saudade <saudade.bonfire.6002@gmail.com>
2020-06-06 12:51:30 +0900

    a

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
   def top
-    if session[:user_id]
-      flash[:notice] = "ログアウトしてください"
-      redirect_to("/tasks/index")
-    end
+    session[:user_id] = nil
   end
 end
